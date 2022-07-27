3a32b98038b9a2e7929d05a6895dcf944089689f
Arthur Weirich <arthur_weirich@hotmail.com>
2022-07-27 17:38:52 -0300

    Exercício sobre soma

---
+++ semana-1/checkpoint-1/segundo.js
@@ -0,0 +1,10 @@
+//Exercício sobre somar 3 números, armazenar em uma variável e imprimir
+
+//Criando variáveis com os números
+let a=5, b=10, c=15
+
+//Variável com a soma
+let soma = a + b + c
+
+//Imprimindo
+console.log(`Soma: ${soma}`)
